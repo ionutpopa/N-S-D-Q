40811285e19933203c2ed4d378afb5b22399a7a5
Ionut-Cristian Popa <ionut.popa9743@yahoo.com>
2021-05-16 21:07:06 +0300

    added question-add-container

--- components/main-view/main-view.js
+++ components/main-view/main-view.js
@@ -1,15 +1,83 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
+  Alert,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
+  Button,
   View,
 } from 'react-native';
+import axios from 'axios';
 
 const Main = () => {
+  const [questions, setQuestions] = useState([]);
+  const initialState = {question: '', answer: ''};
+  const [question, setQuestion] = useState(initialState);
+
+  const [focusInput, setFocusInput] = useState('#7e7e7e');
+
+  const truncate = input =>
+    input.length > 100 ? (
+      <View className="truncate-text-container">
+        <Text>{input.substring(0, 100)} . . .</Text>
+        <Text>click to see the question</Text>
+      </View>
+    ) : (
+      input
+    );
+
+  const onFocusInput = () => {
+    setFocusInput('#272727');
+  };
+
+  const handleChange = text => {
+    setQuestion({...question, text});
+  };
+
+  // const handleSubmit = event => {
+  //   event.preventDefault();
+
+  //   if (!question.question) {
+  //     return;
+  //   }
+
+  //   const postQuestion = async () => {
+  //     try {
+  //       await axios.post('/api/questions', question);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //   postQuestion();
+  //   window.location.reload(false);
+  // };
+
+  // useEffect(() => {
+  //   const getQuestions = async () => {
+  //     try {
+  //       const response = await axios.get("/api/questions");
+  //       setQuestions(response.data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   getQuestions();
+
+  //   if (detect) {
+  //     document.addEventListener("mousedown", handleClickOutside);
+  //   } else {
+  //     document.removeEventListener("mousedown", handleClickOutside);
+  //   }
+
+  //   return () => {
+  //     document.removeEventListener("mousedown", handleClickOutside);
+  //   };
+  // }, [detect]);
+
   return (
     <SafeAreaView>
       <StatusBar />
@@ -27,6 +95,57 @@ const Main = () => {
               of the question because anything it's <Text>anonymized! </Text>
               Enjoy!
             </Text>
+          </View>
+          <View style={styles.addQuestionContainer}>
+            <Text style={styles.addQuestionText}>Create a Question: </Text>
+            <TextInput
+              // eslint-disable-next-line react-native/no-inline-styles
+              style={{
+                borderRadius: 8,
+                padding: 11,
+                borderWidth: 2,
+                borderColor: focusInput,
+              }}
+              onChangeText={text => handleChange(text)}
+              value={question.question}
+              placeholder="add a question!"
+              onFocus={onFocusInput}
+              //onSubmitEditing={handleSubmit}
+            />
+            <View style={styles.submitQuestion}>
+              <Button
+                style={styles.submitQuestionButton}
+                title="Add"
+                onPress={() => Alert.alert('add a question button is pressed')}
+              />
+            </View>
+          </View>
+          <View>
+            {questions.map(q => {
+              return (
+                <View
+                  key={q._id}
+                  //to={q.answer === '' ? `/${q._id}/edit` : `/${q._id}`}
+                >
+                  <View className="question-container">
+                    <View className="question">
+                      <Text>Q: </Text>
+                      {truncate(q.question)}
+                    </View>
+                    <View className="answer">
+                      <Text>A: </Text>
+                      {q.answer === '' ? (
+                        <Text className="add-answer">
+                          Open to add an answer
+                        </Text>
+                      ) : (
+                        truncate(q.answer)
+                      )}
+                    </View>
+                  </View>
+                </View>
+              );
+            })}
           </View>
         </View>
       </ScrollView>
@@ -57,18 +176,43 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   smallContainer: {
-    backgroundColor: '#f2f2f2',
+    backgroundColor: '#f1f1f1',
     paddingVertical: 10,
     paddingHorizontal: 20,
     marginHorizontal: 10,
     alignItems: 'center',
     textAlign: 'center',
     marginVertical: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
   },
   smallText: {
     fontSize: 14,
     fontWeight: '500',
     letterSpacing: 1,
+  },
+  addQuestionContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addQuestionText: {
+    fontSize: 18,
+  },
+  submitQuestion: {
+    backgroundColor: '#ff9900',
+  },
+  submitQuestionButton: {
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    fontSize: 15,
+    borderWidth: 2,
   },
 });
 
